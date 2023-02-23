8c4a79491485d28c7ff5017fdaa91001ec6da0ce
Leticia <vargasj.leti@gmail.com>
2023-02-23 11:37:11 -0600

    agregamos cards e imagenes

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const cardTop = document.querySelector('#cardTop').content
+const contenido = document.querySelector('#contenido')
+const fragment = document.createDocumentFragment()
+let topTwoHundred = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadMusicData()
+})
+
+const loadMusicData = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'adc945c8cfmshc079843a7036b3cp1f019ejsnb4f51094f00d',
+            'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
+        .then(response => response.json())
+        .then(response => {
+            topTwoHundred = response
+            creaCards()
+            console.log('canciones', topTwoHundred)
+        })
+        .catch(err => console.error(err));
+}
+
+const creaCards = () => {
+   topTwoHundred.forEach((song) => {
+        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
+        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        const clone = cardTop.cloneNode(true)
+        fragment.appendChild(clone)
+   })
+   contenido.appendChild(fragment)
+}
