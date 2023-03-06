2203b4638456a3fce626cdc2e76782679c5fb19a
Leticia <vargasj.leti@gmail.com>
2023-03-06 11:30:00 -0600

    implementacion de diseño de cartas y buscador

--- js/index.js
+++ js/index.js
@@ -1,17 +1,18 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
 let topTwoHundred = []
 
 document.addEventListener('DOMContentLoaded', () => {
-    loadMusicData()
+    loadMusic()
 })
 
-const loadMusicData = () => {
+const loadMusic = () => {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'adc945c8cfmshc079843a7036b3cp1f019ejsnb4f51094f00d',
+            'X-RapidAPI-Key': 'eeb1dddf5bmsh2fbafb6cbe18295p148717jsn5d2c74b6f213',
             'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
         }
     };
@@ -27,11 +28,27 @@ const loadMusicData = () => {
 }
 
 const creaCards = () => {
-   topTwoHundred.forEach((song) => {
+    topTwoHundred.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            //console.log(index, size)
+            if(index === size - 1){
+                artists += item.name
+            } else {
+            artists += item.name + ' / '
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
+
         const clone = cardTop.cloneNode(true)
         fragment.appendChild(clone)
-   })
-   contenido.appendChild(fragment)
+    })
+    contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () => {
+    console.log('tecla', btnBuscar.value)
+})
